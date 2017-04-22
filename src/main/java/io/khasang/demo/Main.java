8b7067b07ce6e451e5f0ccdc4a310a096194614f
Azon <azon.sk@gmail.com>
2017-04-22 19:50:45 +0300

    add count

--- src/main/java/io/khasang/demo/Main.java
+++ src/main/java/io/khasang/demo/Main.java
@@ -2,6 +2,7 @@ package io.khasang.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numberCount = 2;
+        System.out.println("Hello World!" + numberCount);
     }
 }
